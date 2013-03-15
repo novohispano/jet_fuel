cbe734a7aa46c700e408a58219da1c7babc96528
novohispano@gmail.com <novohispano@gmail.com>
2013-03-15 14:45:22 -0600

    Maybe working? DB fixed again

--- db/migrate/20130315191804_changing_ids_to_integers.rb
+++
@@ -1,8 +0,0 @@
-class ChangingIdsToIntegers < ActiveRecord::Migration
-  def change
-    change_column :requests, :user_url_id, :integer
-    change_column :requests, :url_id, :integer    
-    change_column :user_urls, :user_id, :integer    
-    change_column :urls, :user_id, :integer
-  end
-end
